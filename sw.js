a964932c04017e28029c39b876a5fa0ba0c6f8e4
dt2398 <dt2398>
2026-08-22 13:10:29 +0900

    demo_pwa_v17

---
+++ sw.js
@@ -0,0 +1,62 @@
+const CACHE_NAME = 'lemon-cozybook-demo-pwa-v17-1';
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/page1.html',
+  '/page2.html',
+  '/page3.html',
+  '/page4.html',
+  '/page5.html',
+  '/flower-data.js',
+  '/lemon-favicon.png',
+  '/lemon-banner-pc.png',
+  '/lemon-banner-mobile.png',
+  '/back-button.png',
+  '/guide-default.png',
+  '/guide-hover.png',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const request = event.request;
+  if (request.method !== 'GET') return;
+
+  const url = new URL(request.url);
+  if (url.origin !== self.location.origin || url.pathname.startsWith('/api/')) return;
+
+  if (request.mode === 'navigate') {
+    event.respondWith(
+      fetch(request)
+        .then((response) => {
+          const copy = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+          return response;
+        })
+        .catch(async () => (await caches.match(request)) || (await caches.match('/index.html')))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(request).then((cached) => cached || fetch(request).then((response) => {
+      if (response && response.status === 200 && response.type === 'basic') {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(request, copy));
+      }
+      return response;
+    }))
+  );
+});
