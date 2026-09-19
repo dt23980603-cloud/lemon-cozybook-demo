8b6ecefeeaea121bbff45ef6dad6137b2ee5feac
dt2398 <dt2398>
2026-09-19 11:27:50 +0900

    demo_v4.20

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'lemon-demo-v4.19-update-prompt';
+const CACHE_NAME = 'lemon-demo-v4.20-update-prompt';
 const APP_SHELL = [
   '/',
   '/index.html',
@@ -65,7 +65,7 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'APPLY_UPDATE' && event.data.version === 'v4.19') {
+  if (event.data && event.data.type === 'APPLY_UPDATE' && event.data.version === 'v4.20') {
     self.skipWaiting();
   }
 });
